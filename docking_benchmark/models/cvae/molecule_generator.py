Fix CVAE gradient descent

docking_benchmark/models/cvae/molecule_generator.py
@@ -15,14 +15,16 @@ class CVAEGradientGenerator:
     def __init__(self, pretrained_path, dataset,
                  latent=196, batch_size=256,
                  fine_tune_epochs=5, fine_tune_test_size=0.1,
-                 fit_epochs=30, descent_iterations=50, descent_lr=0.01,
+                 mlp_fit_epochs=50, mlp_test_size=0.1,
+                 descent_iterations=50, descent_lr=0.05,
                  mode='minimize'):
         self.latent = latent
         self.fine_tune_epochs = fine_tune_epochs
         self.fine_tune_test_size = fine_tune_test_size
         self.cvae = vae_utils.VAEUtils(directory=pretrained_path)
         self.batch_size = batch_size
-        self.fit_epochs = fit_epochs
+        self.mlp_fit_epochs = mlp_fit_epochs
+        self.mlp_test_size = mlp_test_size
         self.descent_iterations = descent_iterations
         self.descent_lr = descent_lr
         smiles, scores = dataset
@@ -89,7 +91,9 @@ class CVAEGradientGenerator:
         self._fine_tune()
 
         predicted_docking_function = MLPPredictedDockingScore(
-            self.dataset, input_dim=self.latent, to_latent_fn=self._smiles_to_latent_fn()
+            self.dataset, input_dim=self.latent,
+            epochs=self.mlp_fit_epochs, test_fraction=self.mlp_test_size,
+            to_latent_fn=self._smiles_to_latent_fn(),
         )
 
         valid_samples = []
@@ -97,6 +101,9 @@ class CVAEGradientGenerator:
 
         while len(valid_samples) < number_molecules:
             latents = np.random.normal(size=(self.batch_size, self.latent))
+
+            before = [predicted_docking_function.latent_score(latents[i].reshape(1, -1))
+                      for i in range(self.batch_size)]
 
             for _ in range(self.descent_iterations):
                 latents += predicted_docking_function.gradient(latents) * self.descent_lr
@@ -107,6 +114,7 @@ class CVAEGradientGenerator:
             for i, smi in enumerate(smiles):
                 if smi is not None and is_valid(smi):
                     latent_score = predicted_docking_function.latent_score(latents[i].reshape(1, -1))
+                    logger.info(f'Optimized from {before[i]} to {latent_score}')
                     valid_samples.append((smi, latent_score))
 
             total_sampled += self.batch_size

docking_benchmark/models/cvae/vae_utils.py
@@ -1,8 +1,6 @@
 import os
-import random
 
 import numpy as np
-import pandas as pd
 import tensorflow as tf
 import yaml
 from keras import backend as K
@@ -31,19 +29,17 @@ def kl_loss(truth_dummy, x_mean_log_var_output):
     return - 0.5 * K.mean(1 + x_log_var - K.square(x_mean) - K.exp(x_log_var), axis=-1)
 
 
-class VAEUtils(object):
+class VAEUtils:
     def __init__(self,
                  exp_file='exp.json',
                  encoder_file=None,
                  decoder_file=None,
                  directory=None,
-                 lr=2e-4,
-                 estimate_estandard=True):
+                 lr=2e-4):
         # files
         if directory is not None:
             curdir = os.getcwd()
             os.chdir(os.path.join(curdir, directory))
-            # exp_file = os.path.join(directory, exp_file)
 
         # load parameters
         self.params = hyperparameters.load_params(exp_file, False)
@@ -96,47 +92,6 @@ class VAEUtils(object):
         if self.params['do_prop_pred']:
             self.property_predictor = load_property_predictor(self.params)
 
-        self.data = None
-
-        if estimate_estandard:
-            # Load data without normalization as dataframe
-            df = pd.read_csv(self.params['data_file'])
-            df.iloc[:, 0] = df.iloc[:, 0].str.strip()
-            df = df[df.iloc[:, 0].str.len() <= self.params['MAX_LEN']]
-            self.smiles = df.iloc[:, 0].tolist()
-            if df.shape[1] > 1:
-                self.data = df.iloc[:, 1:]
-
-            self.estimate_estandarization()
-
-        if directory is not None:
-            os.chdir(curdir)
-
-    def estimate_estandarization(self):
-        logger.info('Standarization: estimating mu and std values...')
-
-        # sample Z space
-        smiles = self.random_molecules(size=50000)
-        batch = 2500
-        latents = np.zeros((len(smiles), self.params['hidden_dim']))
-
-        for chunk in self.chunks(list(range(len(smiles))), batch):
-            sub_smiles = [smiles[i] for i in chunk]
-            one_hot = self.smiles_to_hot(sub_smiles)
-            latents[chunk, :] = self.encode(one_hot, False)
-
-        self.mu = np.mean(latents, axis=0)
-        self.std = np.std(latents, axis=0)
-        self.Z = self.standardize_z(latents)
-
-        logger.info('Standardization finished')
-
-    def standardize_z(self, z):
-        return (z - self.mu) / self.std
-
-    def unstandardize_z(self, z):
-        return (z * self.std) + self.mu
-
     def perturb_z(self, z, noise_norm, constant_norm=False):
         if noise_norm > 0.0:
             noise_vec = np.random.normal(0, 1, size=z.shape)
@@ -150,28 +105,19 @@ class VAEUtils(object):
         else:
             return z
 
-    def enc_dec_functions(self, standardized=True):
-        logger.info('Using standardized functions? {}'.format(standardized))
-
+    def enc_dec_functions(self):
         if not self.params['do_tgru']:
-            def decode(z, standardize=standardized):
-                if standardize:
-                    z = self.unstandardize_z(z)
-
+            def decode(z):
                 return self.dec.predict(z)
         else:
-            def decode(z, standardize=standardized):
+            def decode(z):
                 fake_shape = (z.shape[0], self.params['MAX_LEN'], self.params['NCHARS'])
                 fake_in = np.zeros(fake_shape)
 
-                if standardize:
-                    z = self.unstandardize_z(z)
-
                 return self.dec.predict([z, fake_in])
 
-        def encode(one_hot, standardize=standardized):
-            latent = self.enc.predict(one_hot)[0]
-            return self.standardize_z(latent) if standardize else latent
+        def encode(one_hot):
+            return self.enc.predict(one_hot)[0]
 
         return encode, decode
 
@@ -205,12 +151,6 @@ class VAEUtils(object):
             smiles = [s.strip() for s in smiles]
 
         return smiles
-
-    def random_molecules(self, size=None):
-        if size is None:
-            return self.smiles
-        else:
-            return random.sample(self.smiles, size)
 
     @staticmethod
     def chunks(l, n):
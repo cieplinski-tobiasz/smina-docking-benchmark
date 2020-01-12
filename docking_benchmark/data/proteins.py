d7aeaa77c211b2dd39a3ac5c82677bac50420e1a
Tobiasz Ciepliński <cieplinski.t@gmail.com>
2020-01-12 22:59:12 +0100

    Add docs and more tests for Protein

--- docking_benchmark/data/proteins.py
+++ docking_benchmark/data/proteins.py
@@ -1,5 +1,6 @@
 import json
 import os
+from typing import List
 
 import pandas as pd
 
@@ -35,9 +36,73 @@ class Datasets:
 
 
 class Protein:
-    _METADATA_FILENAME = 'metadata.json'
+    """Container for protein-related data.
 
-    def __init__(self, name, directory):
+    The class provides required protein information
+    for SMINA docking software.
+
+    Single protein's data must be put into one directory.
+    This directory *must* contain a `metadata.json` file
+    and a *single* file with .pdb/.pdbqt extension.
+
+    `metadata.json` stores basic information about protein,
+    such as its pocket center coordinates and available datasets
+    associated with it.
+
+    See the example below for `metadata.json` file structure.
+
+    Example `metadata.json` file:
+    ```json
+    {
+        "pocket_center": [0.0, 0.0, 0.0],
+        "datasets": {
+            "first_dataset": {
+                "path": "datasets/first.csv",
+                "smiles_column": "smi",
+                "score_column": "activity"
+            },
+            "second_dataset": {
+                "path": "datasets/second.csv"
+            }
+        }
+    }
+    ```
+
+    `pocket_center` field *is required*.
+    It must be a list of three numbers - coordinates used for docking.
+
+    `datasets` key is optional.
+    Each entry in the `datasets` dictionary *must* have `path` field present.
+    `smiles_column` and `score_column` are optional.
+
+    Attributes:
+        name (str): Name of the protein, e.g. '5ht1b'.
+        directory: Path to the directory containing protein related files.
+        path: Path to the protein file.
+        metadata (dict): Dictionary with parsed metadata.json file (see above for description).
+        datasets (Datasets): Available datasets for the protein.
+    """
+    _METADATA_FILENAME = 'metadata.json'
+    _POCKET_CENTER_LENGTH = 3
+
+    def __init__(self, name: str, directory):
+        """Creates a Protein from given directory.
+
+        See the class-level docs for required directory structure.
+
+        Args:
+            name (str): Name of the protein.
+            directory: Path to the directory containing protein related files.
+
+        Raises:
+            ValueError: If directory does not exist,
+                        does not contain metadata.json file,
+                        contains invalid metadata.json file
+                        or does not contain a single .pdb/.pdbqt file.
+        """
+        if not os.path.exists(directory):
+            raise ValueError(f'Directory {directory} does not exist')
+
         self.name = name
         self.directory = directory
         self.path = self._load_protein_file_path()
@@ -45,6 +110,18 @@ class Protein:
         self.datasets = Datasets(self)
 
     def _load_protein_file_path(self):
+        """Returns protein file path from provided directory.
+
+        The method looks for .pdb/.pdbqt file in provided directory.
+        No validity check of the file is performed.
+
+        Raises:
+            ValueError: If no, or more than one .pdb/.pdbqt file
+                        is present in the directory.
+
+        Returns:
+            Path to .pdb/.pdbqt file.
+        """
         protein_files = [entry for entry in os.listdir(self.directory)
                          if any(entry.endswith(fmt) for fmt in PROTEIN_FORMATS)]
 
@@ -54,17 +131,41 @@ class Protein:
 
         return os.path.join(self.directory, protein_files[0])
 
-    def _load_metadata(self):
-        with open(os.path.join(self.directory, self.__class__._METADATA_FILENAME)) as metadata_file:
+    def _load_metadata(self) -> dict:
+        """Loads and parses `metadata.json` file from provided directory.
+
+        Raises:
+            ValueError: If `metadata.json file` does not exist or is invalid.
+
+        Returns:
+            dict: Parsed `metadata.json` file.
+        """
+        metadata_path = os.path.join(self.directory, self.__class__._METADATA_FILENAME)
+
+        if not os.path.exists(metadata_path):
+            raise ValueError('No metadata.json file for ' + self.name)
+
+        with open(metadata_path) as metadata_file:
             metadata = json.load(metadata_file)
 
             if 'pocket_center' not in metadata:
-                raise ValueError('Protein ' + self.name + ' metadata must contain pocket_center key')
+                raise ValueError('Protein ' + self.name +
+                                 ' metadata must contain pocket_center key')
+
+            if len(metadata['pocket_center']) != self._POCKET_CENTER_LENGTH:
+                raise ValueError('Pocket center for ' + self.name +
+                                 'must be a list of three floats')
+
+            for coordinate in metadata['pocket_center']:
+                if type(coordinate) is not float:
+                    raise ValueError('Pocket center for ' + self.name +
+                                     'must be a list of three floats')
 
             return metadata
 
     @property
-    def pocket_center(self):
+    def pocket_center(self) -> List[float]:
+        """list[float]: Pocket center coordinates used for docking"""
         return self.metadata['pocket_center']
 
 
--- docking_benchmark/tests/data/proteins/test_proteins.py
+++ docking_benchmark/tests/data/proteins/test_proteins.py
@@ -30,6 +30,39 @@ def test_protein_init_raises_when_metadata_has_no_pocket_center(tmp_path):
         proteins.Protein('test_name', tmp_path)
 
 
+def test_protein_init_raises_when_pocket_center_has_invalid_length(tmp_path):
+    dummy_protein_file = tmp_path / 'protein_file.pdb'
+    dummy_protein_file.write_text('dummy')
+    metadata_file = tmp_path / 'metadata.json'
+    metadata_file.write_text('{"pocket_center": [0.0, 0.0]}')
+
+    with pytest.raises(ValueError):
+        proteins.Protein('test_name', tmp_path)
+
+
+def test_protein_init_raises_when_pocket_center_is_not_coordinates(tmp_path):
+    dummy_protein_file = tmp_path / 'protein_file.pdb'
+    dummy_protein_file.write_text('dummy')
+    metadata_file = tmp_path / 'metadata.json'
+    metadata_file.write_text('{"pocket_center": ["f"]}')
+
+    with pytest.raises(ValueError):
+        proteins.Protein('test_name', tmp_path)
+
+
+def test_protein_init_raises_if_directory_does_not_exist():
+    with pytest.raises(ValueError):
+        proteins.Protein('test_name', '/no/such/directory')
+
+
+def test_protein_init_raises_if_metadata_file_does_not_exist(tmp_path):
+    dummy_protein_file = tmp_path / 'protein_file.pdb'
+    dummy_protein_file.write_text('dummy')
+    
+    with pytest.raises(ValueError):
+        proteins.Protein('test_name', tmp_path)
+
+
 def test_protein_init_happy_path(tmp_path):
     dummy_protein_file = tmp_path / 'protein_file.pdb'
     dummy_protein_file.write_text('dummy')

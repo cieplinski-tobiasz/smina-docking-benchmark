2fb069c6a29e1087485c5fc55789e0ceae323d2f
Tobiasz Ciepliński <cieplinski.t@gmail.com>
2020-10-19 23:27:51 +0200

    Install right openbabel for syscall

--- docking_benchmark/install_conda_env.sh
+++ docking_benchmark/install_conda_env.sh
@@ -1,6 +1,7 @@
 conda install python=3.6 
 conda install -c conda-forge tensorflow=1.1.0 
-conda install scikit-learn nltk=3.4.4 babel pandas h5py
+conda install scikit-learn nltk=3.4.4 pandas h5py
+conda install -c conda-forge openbabel
 conda install -c bioconda smina
 conda install -c rdkit rdkit
 pip install Keras==2.0.7
